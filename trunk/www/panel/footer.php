c4751a583991c4341bb45a66dd92ec1c33683950
chenzz <chenzz@9c4c1619-c41c-0410-ae47-27ddc40dfa52>
2006-09-08 10:11:30 +0000

    the same footer

--- trunk/www/panel/footer.php
+++ trunk/www/panel/footer.php
@@ -1,8 +1,6 @@
-<p>&nbsp;</p>
-</div></div>
-<div id="divFoot">
- Copyright &copy; 2001-2005 <a href="mailto:control@cn-bbs.org">cn.bbs.* Administrative Group</a>. All rights reserved. <br>
- Powered by <a href="http://www.kernel.org">Linux</a>, <a href="http://www.apache.org">Apache</a>, <a href="http://www.php.net">PHP</a>, <a href="http://www.mysql.com">MySQL</a>, <a href="http://www.vim.org">Vim</a>.
 </div>
+</div>
+<div id="divFoot">
+	Copyright © 2001-2006 <a href="mailto:control@cn-bbs.org">cn.bbs.* Administrative Group</a>. All rights reserved.</div>
 </body>
 </html>
