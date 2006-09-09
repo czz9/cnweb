04a11257d597af94706a42b1c5543267b873be91
pig2532 <pig2532@9c4c1619-c41c-0410-ae47-27ddc40dfa52>
2006-09-09 12:44:00 +0000

    display server status

--- www/serverlist.php
+++ www/serverlist.php
@@ -3,16 +3,22 @@
 <h5>updated on <?php print date('r'); ?></h5>
 <?php
 	require("dbconf.php");
+	
+	$ststr[0] = "<span style=\"color:#0000FF\">未知</span>";
+	$ststr[1] = "<span style=\"color:#009900\">正常</span>";
+	$ststr[2] = "<span style=\"color:#999900\">暂停</span>";
+	$ststr[3] = "<span style=\"color:#FF0000\">故障</span>";
+	
 	mysql_connect(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD);
 	mysql_select_db(MYSQL_DB);
-	$query = "SELECT `name`,`url`,`host`,`comment` FROM `_news_srv` ORDER BY `name` ASC";
+	$query = "SELECT * FROM `_news_srv` ORDER BY `name` ASC";
 	$result = mysql_query($query);
 ?>
 <table border="1">
-<tr align="center"><th>服务器域名</th><th>FQDN/IP</th><th>服务对象</th></tr>
+<tr align="center"><th>服务器域名</th><th>状态</th><th>FQDN/IP</th><th>服务对象</th></tr>
 <?php
 	while($record = mysql_fetch_array($result)) {
-		print("<tr><td><a href=" . $record["url"] . ">" . $record["name"] . "</a></td><td>" . $record["host"] . "</td><td>" . $record["comment"] . "</td></tr>\n");
+		print("<tr><td><a href=" . $record["url"] . ">" . $record["name"] . "</a></td><td>" . $ststr[$record["status"]] . "</td><td>" . $record["host"] . "</td><td>" . $record["comment"] . "</td></tr>\n");
 	}
 ?>
 </table>
