0cfcf70e2802edc6977bd215a32813fef8ebed21
chenzz <chenzz@9c4c1619-c41c-0410-ae47-27ddc40dfa52>
2006-09-09 01:31:41 +0000

    sort bye group name

--- trunk/www/newsgroup.php
+++ trunk/www/newsgroup.php
@@ -6,7 +6,7 @@
 	require("dbconf.php");
 	mysql_connect(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD);
 	mysql_select_db(MYSQL_DB);
-	$query = "SELECT `name`,`title` FROM `_news_grp` ORDER BY `id` ASC";
+	$query = "SELECT `name`,`title` FROM `_news_grp` ORDER BY `name` ASC";
 	$result = mysql_query($query);
 	while($record = mysql_fetch_array($result)) {
 		print($record["name"] . "\t" . $record["title"] . "\n");
