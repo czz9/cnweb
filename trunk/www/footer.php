0345b319020ecf0a152ae00c855ac6ada2cc87de
chenzz <chenzz@9c4c1619-c41c-0410-ae47-27ddc40dfa52>
2008-01-02 10:09:24 +0000

    s/2006/2008/

--- trunk/www/footer.php
+++ trunk/www/footer.php
@@ -1,13 +1,15 @@
-<div id="divFoot">Copyright © 2001-2006 <a href="mailto:control@cn-bbs.org">cn.bbs.* Administrative Group</a>. All rights reserved.<br />
+<div id="divFoot">Copyright © 2001-2008 <a href="mailto:control@cn-bbs.org">cn.bbs.* Administrative Group</a>. All rights reserved.<br />
 <a href="http://www.miibeian.gov.cn/">京ICP备05079017号</a></div>
 <script language="javascript" src="/js/awstats_misc_tracker.js" type="text/javascript">
-</script><noscript><img src="/js/awstats_misc_tracker.js?nojs=y" height="0" width="0" border="0" style="display: none" alt="" /></noscript><script src="http://www.google-analytics.com/urchin.js"
-type="text/javascript">
-</script><script type="text/javascript">
-//<![CDATA[
-_uacct = "UA-251408-2";
-urchinTracker();
-//]]>
+</script><noscript><img src="/js/awstats_misc_tracker.js?nojs=y" height="0" width="0" border="0" style="display: none" alt="" /></noscript>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-251408-2");
+pageTracker._initData();
+pageTracker._trackPageview();
 </script>
 </body>
 </html>
